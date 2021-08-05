8eebe3ec1c9d99ffe098a7d2c5ed4884adfabcd6
clairebookworm <clara32356@gmail.com>
2021-08-05 12:19:55 -0700

    First commit

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -3,22 +3,39 @@ import { GetServerSideProps } from "next"
 import ReactMarkdown from "react-markdown"
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
+import prisma from '../../lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = {
-    id: 1,
-    title: "Prisma is the perfect ORM for Next.js",
-    content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-    published: false,
-    author: {
-      name: "Nikolas Burk",
-      email: "burk@prisma.io",
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(params?.id) || -1,
     },
-  }
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
   return {
     props: post,
-  }
-}
+  };
+};
+
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const post = {
+//     id: 1,
+//     title: "Prisma is the perfect ORM for Next.js",
+//     content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+//     published: false,
+//     author: {
+//       name: "Nikolas Burk",
+//       email: "burk@prisma.io",
+//     },
+//   }
+//   return {
+//     props: post,
+//   }
+// }
 
 const Post: React.FC<PostProps> = (props) => {
   let title = props.title
